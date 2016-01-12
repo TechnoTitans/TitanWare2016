a18c729e8e69b9e6b6d41617cb0a12bbfd8aacfb
David Luo <davidl2381@gmail.com>
2016-01-12 17:43:04 -0500

    Added Autonomous, DriveTrain, and Sensor classes.

---
+++ src/org/usfirst/frc/team1683/autonomous/Autonomous.java
@@ -0,0 +1,10 @@
+package org.usfirst.frc.team1683.autonomous;
+
+/**
+ * Class to be inherited by all auto classes.
+ * @author David Luo
+ *
+ */
+public abstract class Autonomous {
+	//TODO: make automous
+}
---
+++ src/org/usfirst/frc/team1683/driveTrain/DriveTrain.java
@@ -0,0 +1,10 @@
+package org.usfirst.frc.team1683.driveTrain;
+
+/**
+ * <finish this documentation later>
+ * @author David Luo
+ *
+ */
+public abstract class DriveTrain {
+	//TODO: this class
+}
