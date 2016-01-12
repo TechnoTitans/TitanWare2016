293c686a786d4c1bf081be96ea37e3572b31c8de
David Luo <davidl2381@gmail.com>
2016-01-12 17:30:54 -0500

    Added autonomous, driveTrain, sensors packages. Refactored main class to TechnoTitan.

--- src/org/usfirst/frc/team1683/robot/TechnoTitan.java
+++ src/org/usfirst/frc/team1683/robot/TechnoTitan.java
@@ -13,7 +13,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  * creating this project, you must also update the manifest file in the resource
  * directory.
  */
-public class Robot extends IterativeRobot {
+public class TechnoTitan extends IterativeRobot {
     final String defaultAuto = "Default";
     final String customAuto = "My Auto";
     String autoSelected;
