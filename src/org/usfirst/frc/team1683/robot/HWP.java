4f8a8dea0cd1d4b984bc795e2428add36f404916
David Luo <davidl2381@gmail.com>
2016-01-16 22:58:24 -0500

    Added documetation about HWR and HWP. Added Joysticks to HWR.

--- src/org/usfirst/frc/team1683/robot/HWP.java
+++ src/org/usfirst/frc/team1683/robot/HWP.java
@@ -1,7 +1,14 @@
 package org.usfirst.frc.team1683.robot;
 
+/**
+ * HWP - Stands for HardWare Ports Each variable represents a port on the
+ * RoboRio
+ * 
+ * @author David
+ *
+ */
 public class HWP {
-	
+
 	// Pulse Width Modulation (PWM)
 	public static final int PWM_0 = 0;
 	public static final int PWM_1 = 1;
@@ -13,9 +20,10 @@ public class HWP {
 	public static final int PWM_7 = 7;
 	public static final int PWM_8 = 8;
 	public static final int PWM_9 = 9;
-	
-	//Controller Area Network (CAN)
-	public static final int CAN_0 = 0;	//do not assign to any TalonSRX (default)
+
+	// Controller Area Network (CAN)
+	public static final int CAN_0 = 0; // do not assign to any TalonSRX
+										// (default)
 	public static final int CAN_1 = 1;
 	public static final int CAN_2 = 2;
 	public static final int CAN_3 = 3;
@@ -26,19 +34,19 @@ public class HWP {
 	public static final int ANALOG_1 = 1;
 	public static final int ANALOG_2 = 2;
 	public static final int ANALOG_3 = 3;
-	
+
 	// Relays
 	public static final int RELAY_0 = 0;
 	public static final int RELAY_1 = 1;
 	public static final int RELAY_2 = 2;
 	public static final int RELAY_3 = 3;
-	
+
 	// Joysticks
 	public static final int JOY_0 = 0;
 	public static final int JOY_1 = 1;
 	public static final int JOY_2 = 2;
-	
-	//Digital I/O
+
+	// Digital I/O
 	public static final int DIO_0 = 0;
 	public static final int DIO_1 = 1;
 	public static final int DIO_2 = 2;
@@ -49,10 +57,9 @@ public class HWP {
 	public static final int DIO_7 = 7;
 	public static final int DIO_8 = 8;
 	public static final int DIO_9 = 9;
-	//MXP Expansion port
+	// MXP Expansion port
 	public static final int DIO_10 = 10;
 	public static final int DIO_11 = 11;
 	public static final int DIO_12 = 12;
-	
-	
+
 }
--- src/org/usfirst/frc/team1683/robot/HWR.java
+++ src/org/usfirst/frc/team1683/robot/HWR.java
@@ -1,18 +1,32 @@
 package org.usfirst.frc.team1683.robot;
 
+/**
+ * HWR - Stands for HardWare References. Each variable references a piece of
+ * hardware to a channel or port.
+ * 
+ * @author David
+ *
+ */
 public class HWR {
 
+	// Motors
 	public static final int TALON_0 = HWP.PWM_0;
 	public static final int TALON_1 = HWP.PWM_1;
 	public static final int TALON_2 = HWP.PWM_2;
 	public static final int TALON_3 = HWP.PWM_3;
-	
+
 	public static final int TALON_SRX_0 = HWP.CAN_0;
 	public static final int TALON_SRX_1 = HWP.CAN_1;
 	public static final int TALON_SRX_2 = HWP.CAN_2;
 	public static final int TALON_SRX_3 = HWP.CAN_3;
-	
+
+	// Encoders
 	public static final int ENCODER_0_A = HWP.DIO_0;
 	public static final int ENCODER_0_B = HWP.DIO_1;
-	
+
+	// Joysticks
+	public static final int LEFT_JOYSTICK = HWP.JOY_0;
+	public static final int RIGHT_JOYSTICK = HWP.JOY_1;
+	public static final int AUX_JOYSTICK = HWP.JOY_2;
+
 }
