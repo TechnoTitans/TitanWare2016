2e2f972bbcdce0354472c6ecd911503bc8a170bb
David Luo <davidl2381@gmail.com>
2016-01-25 16:27:28 -0500

    Driver Station and SmartDashboard

---
+++ src/org/usfirst/frc/team1683/driverStation/DriverStation.java
@@ -0,0 +1,22 @@
+package org.usfirst.frc.team1683.driverStation;
+
+import org.usfirst.frc.team1683.robot.HWR;
+
+import edu.wpi.first.wpilibj.Joystick;
+
+public class DriverStation {
+
+	public static final int XAxis = 0;
+	public static final int YAxis = 1;
+	public static final int ZAxis = 2;
+	public static final int dialAxis = 3;
+	
+	public static Joystick leftStick = new Joystick(HWR.LEFT_JOYSTICK);
+	public static Joystick rightStick = new Joystick(HWR.RIGHT_JOYSTICK);
+	public static Joystick auxStick = new Joystick(HWR.AUX_JOYSTICK);
+	
+	public static boolean anitBounce (/*args*/) {
+		return false;
+	}
+
+}
