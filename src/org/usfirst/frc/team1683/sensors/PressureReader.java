b018dce4f26c89d09ac8dc46aa90bad089157a29
Devansh Yerpude <ydev0916@gmail.com>
2016-02-11 18:13:34 -0500

    Made a pressure sensor

---
+++ src/org/usfirst/frc/team1683/sensors/PressureReader.java
@@ -0,0 +1,45 @@
+package org.usfirst.frc.team1683.sensors;
+
+import edu.wpi.first.wpilibj.AnalogInput;
+public class PressureReader implements Sensor {
+	private final double MAX_VOLTAGE = 4.5;
+	private final double MIN_VOLTAGE = 0.5;
+	private final double MIN_PRESSURE= 0;
+	private final double MAX_PRESSURE = 200;
+	private double averageVoltage;
+	private double portNumber;
+	AnalogInput sensor;
+	private double pressure;
+	private final double VOLTAGE_RANGE= MAX_VOLTAGE-MIN_VOLTAGE;
+	private final double PRESSURE_SLOPE= MAX_PRESSURE/VOLTAGE_RANGE;
+
+	public PressureReader(int portNumber) {
+		this.portNumber = portNumber;
+		sensor = new AnalogInput(portNumber);
+	}
+
+	public double getPortNumber() {
+		return portNumber;
+	}
+
+	public double getRaw() {
+		return sensor.getAverageVoltage();
+
+	}
+
+	public double getPressure() {
+		averageVoltage= sensor.getAverageVoltage();
+		if( averageVoltage > MAX_VOLTAGE ){
+			pressure=MAX_PRESSURE;
+		}
+		else if (averageVoltage < MIN_VOLTAGE){
+			pressure=MIN_PRESSURE;
+		}
+		else {
+			pressure= PRESSURE_SLOPE*(averageVoltage-MIN_VOLTAGE);
+		}
+		
+		return pressure;
+		
+	}
+}
