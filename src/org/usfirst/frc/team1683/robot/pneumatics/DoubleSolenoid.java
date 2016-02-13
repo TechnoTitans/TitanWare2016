fecb1bdd841b6278a60edf07b9d2ecc97f8055fc
Devansh Yerpude <ydev0916@gmail.com>
2016-02-13 11:04:20 -0500

    needed to get the code on driverstation (incomplete)

---
+++ src/org/usfirst/frc/team1683/robot/pneumatics/DoubleSolenoid.java
@@ -0,0 +1,9 @@
+package org.usfirst.frc.team1683.robot.pneumatics;
+ 
+public class DoubleSolenoid extends edu.wpi.first.wpilibj.Solenoid  {
+
+	public DoubleSolenoid(int moduleNumber, int channel) {
+		super(moduleNumber, channel);
+		}
+
+}
