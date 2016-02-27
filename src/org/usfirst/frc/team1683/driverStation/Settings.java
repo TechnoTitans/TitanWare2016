f17571aff866d5599ccd89c53416aa5d6625f4fb
David Luo <davidl2381@gmail.com>
2016-02-27 16:02:42 -0500

    Start on centralizing settings.

---
+++ src/org/usfirst/frc/team1683/driverStation/Settings.java
@@ -0,0 +1,22 @@
+package org.usfirst.frc.team1683.driverStation;
+
+import java.util.HashMap;
+
+public class Settings {
+	
+	public static HashMap<String, Double> thing;
+	public static double angleMotorP;
+	public static double angleMotorI;
+	public static double angleMotorD;
+	
+	public static double shooterMotorP;
+	public static double shooterMotorI;
+	public static double shooterMotorD;
+	public static double shooterMotorF;
+	
+	public static double shooterSensitivity;
+	
+	public static void updateSettings() {
+		
+	}
+}
