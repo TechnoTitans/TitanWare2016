4e85786d17c69b305e83cfb368edea230d2f1be8
Sneha Kadiyala <johndoe@example.com>
2016-03-15 17:07:46 -0400

    Autonomous and tele-op angle control

--- src/org/usfirst/frc/team1683/shooter/Shooter.java
+++ src/org/usfirst/frc/team1683/shooter/Shooter.java
@@ -11,6 +11,7 @@ import org.usfirst.frc.team1683.sensors.LinearActuator;
 import org.usfirst.frc.team1683.vision.FindGoal;
 
 public class Shooter {
+	public static final double JOYSTICK_TOLERANCE = 0.05;
 	public static final double TOLERANCE = 1.0;
 	public static final double SPEED_TOLERANCE = 0.05;
 	public static final double SHOOT_SPEED = SmartDashboard.getDouble("Shooter speed");
@@ -19,16 +20,31 @@ public class Shooter {
 
 	private final double MAX_ENCODER_COUNT = 512;
 	private final double MIN_ENCODER_COUNT = 0;
-	private final double MAX_ANGLE = 46;
-	private final double MIN_ANGLE = -4;
+	private final double MAX_ANGLE = 70;
+	private final double MIN_ANGLE = 0;
 	private final double POSITION_TO_ANGLE_COEFFICENT = (MAX_ANGLE - MIN_ANGLE)
 			/ (MAX_ENCODER_COUNT - MIN_ENCODER_COUNT);
 	private final double ANGLE_TO_POSITION_COEFFICENT = (MAX_ENCODER_COUNT - MIN_ENCODER_COUNT)
 			/ (MAX_ANGLE - MIN_ANGLE);
 
+	public static final double MAX_DISTANCE = 136;
+	public static final double MIN_DISTANCE = 80.6;
 	public final static double FORWARD_LIMIT_ANGLE = 0;
-	public static final double  BACK_LIMIT_ANGLE = 63;
+	public static final double BACK_LIMIT_ANGLE = 70;
 	public static final int ALLOWABLE_ERROR = 400; // find num
+	
+
+	public static final double INTAKE_SPEED = -2000;
+	private static final double LOW_GOAL_ANGLE = 30; // Pick better angle
+	private static final double LOW_GOAL_SPEED = 3500;
+	private static final double DEFAULT_SPEED = 3500;
+	public static final double ANGLE_OFFSET = 0; //change based on shooter mounting
+	
+	public static final double TARGET_OVERHANG = 5;
+	public static final double TARGET_HEIGHT = 97;
+	public static final double CAMERA_HOR_OFF = 8;
+	public static final double CAMERA_VER_OFF = 13;
+	
 
 	private InputFilter inputFilter;
 
@@ -44,16 +60,15 @@ public class Shooter {
 
 	Solenoid shootPiston;
 
+	public double getDistance = 20; // WE NEED TO REPLACE THIS WITH YI'S
+									// DISTANCE METHOD
+
 	private boolean isCreated = false;
 	private State presentTeleOpState;
-	public static final double INTAKE_SPEED = -2000;
-	private static final double INTAKE_ANGLE = 0; // Pick better angle
-	private static final double HOLD_SPEED = 0;
 
 	public enum State {
 		INTAKE, HOLD, SHOOT;
 	}
-
 
 	public Shooter(int leftChannel, int rightChannel, int angleMotorChannel, Solenoid shootPiston, FindGoal vision) {
 		this(leftChannel, rightChannel, angleMotorChannel, shootPiston);
@@ -62,24 +77,21 @@ public class Shooter {
 	}
 
 	public Shooter(int leftChannel, int rightChannel, int angleMotorChannel, Solenoid shootPiston) {
-		this.angleMotor = new TalonSRX(angleMotorChannel, false);;
+		this.angleMotor = new TalonSRX(angleMotorChannel, false);
 		this.leftMotor = new TalonSRX(leftChannel, false);
 		this.rightMotor = new TalonSRX(rightChannel, false);
 		this.shootPiston = shootPiston;
 
-		
 		this.angleMotor.PIDInit();
 		this.leftMotor.PIDInit();
 		this.rightMotor.PIDInit();
-		
+
 		// this.angleMotor.configEncoderCodesPerRev(4096);
 
 		this.angleMotor.calibrate();
 		this.angleMotor.setAllowableClosedLoopErr(0);
-		this.angleMotor.enableBrakeMode(true);
-//		this.angleMotor.enableLimitSwitch(true, true);
+		// this.angleMotor.enableLimitSwitch(true, true);
 
-		
 		this.leftMotor.enableBrakeMode(false); // coast mode
 		this.rightMotor.enableBrakeMode(false); // coast mode
 		this.leftMotor.reverseSensor(false);
@@ -87,12 +99,13 @@ public class Shooter {
 
 		inputFilter = new InputFilter(SmartDashboard.getDouble("Shooter K"));
 
-//		actuator = new LinearActuator(actuatorChannel, false);
+		// actuator = new LinearActuator(actuatorChannel, false);
 
 	}
 
 	/**
 	 * returns scaled joystick output
+	 * 
 	 * @return
 	 */
 	public double getJoystickAngle() {
@@ -132,6 +145,34 @@ public class Shooter {
 	}
 
 	/**
+	 * takes in distance from vision in inches
+	 * @param visDist
+	 * @return
+	 */
+	public double getFloorDistance(double visDist) {
+		return Math.sqrt(Math.pow(visDist, 2) - Math.pow(TARGET_HEIGHT - CAMERA_VER_OFF, 2)) + CAMERA_HOR_OFF + TARGET_OVERHANG;
+	}
+
+	public double getSpeed() {
+		if (getAngle() < LOW_GOAL_ANGLE)
+			return LOW_GOAL_SPEED;
+		else if (getDistance < MAX_DISTANCE && getDistance > MIN_DISTANCE)
+			return 0.0001054 * Math.pow(getDistance, 4) - 0.04996 * Math.pow(getDistance, 3)
+					+ 8.614 * Math.pow(getDistance, 2) - 632.4 * getDistance + 2.083 * Math.pow(10, 4);
+		else
+			return DEFAULT_SPEED;
+	}
+
+	public double getAngle() {
+		if (Math.abs(DriverStation.auxStick.getRawAxis(DriverStation.YAxis)) > JOYSTICK_TOLERANCE)
+			return getJoystickAngle() ;
+		else if (getDistance < MAX_DISTANCE && getDistance > MIN_DISTANCE)
+			return 0.0007799 * Math.pow(getDistance, 2) - 0.3196 * getDistance + 89.48;
+		else
+			return getJoystickAngle();
+	}
+
+	/**
 	 * Set shooter to absolute position angle
 	 * 
 	 * @param angle
@@ -143,7 +184,7 @@ public class Shooter {
 			angle = BACK_LIMIT_ANGLE;
 		}
 
-		updateSettings();
+		updatePIDF();
 
 		SmartDashboard.sendData("Shooter Set Angle", angle);
 
@@ -151,9 +192,9 @@ public class Shooter {
 
 		SmartDashboard.sendData("Shooter Filtered Angle", angle);
 
-//		angle = angle * ANGLE_TO_POSITION_COEFFICENT;
+		// angle = angle * ANGLE_TO_POSITION_COEFFICENT;
 
-//		angleMotor.PIDPosition(angle);
+		// angleMotor.PIDPosition(angle);
 		angleMotor.PIDAngle(angle);
 	}
 
@@ -161,42 +202,44 @@ public class Shooter {
 	 * TeleOp
 	 */
 	public void shootMode() {
-		updateSettings();
+		updatePIDF();
+		updatePrefs();
 
 		stateSwitcher(isCreated);
 
-		//angleClimberPistons();
+		// angleClimberPistons();
 
 		if (presentTeleOpState == State.SHOOT && DriverStation.auxStick.getRawButton(HWR.SHOOT_BALL))
 			shootBall();
 		else
 			resetShootPistons();
-//		angleShooter(SmartDashboard.getDouble("Shooter Target Angle"));
+		// angleShooter(getAngle(SmartDashboard.getDouble("Target Distance")));
 
+		updatePrefs();
 		report();
 	}
 
-	public void intake(double angle) {
+	public void intake() {
 		leftMotor.PIDSpeed(INTAKE_SPEED);
 		rightMotor.PIDSpeed(INTAKE_SPEED);
-		angleShooter(angle);
+		angleShooter(getAngle());
 	}
 
-	public void hold(double angle) {
+	public void hold() {
 		leftMotor.stop();
 		rightMotor.stop();
 		// leftMotor.PIDSpeed(0);
 		// rightMotor.PIDSpeed(0);
-		angleShooter(angle);
+		angleShooter(getAngle());
 	}
 
-	public void shoot(double speed, double angle) {
+	public void shoot(double speed) {
 		leftMotor.PIDSpeed(speed);
 		rightMotor.PIDSpeed(speed);
-		angleShooter(angle);
+		angleShooter(getAngle());
 	}
 
-	public void updateSettings() {
+	public void updatePIDF() {
 		Settings.updateSettings();
 
 		leftMotor.setP(Settings.shooterMotorP);
@@ -208,24 +251,20 @@ public class Shooter {
 		rightMotor.setI(Settings.shooterMotorI);
 		rightMotor.setD(Settings.shooterMotorD);
 		rightMotor.setF(Settings.shooterMotorF);
-		
+
 		leftMotor.setCloseLoopRampRate(Settings.shooterRampRate);
 		rightMotor.setCloseLoopRampRate(Settings.shooterRampRate);
 
 		angleMotor.setP(Settings.angleMotorP);
 		angleMotor.setI(Settings.angleMotorI);
 		angleMotor.setD(Settings.angleMotorD);
-		
-		angleMotor.setAllowableClosedLoopErr(Settings.angleAllowableClosedLoopErr);
-		
-		inputFilter.setFilterK(Settings.shooterK);
 	}
 
 	public void stateSwitcher(boolean isCreated) {
 		String state = "out";
 		State nextState;
 
-		updateSettings();
+		updatePIDF();
 
 		if (!isCreated) {
 			presentTeleOpState = State.HOLD;
@@ -234,7 +273,7 @@ public class Shooter {
 		switch (presentTeleOpState) {
 		case HOLD: {
 			state = "HOLD";
-			hold(getJoystickAngle());
+			hold();
 			nextState = State.HOLD;
 			if (DriverStation.auxStick.getRawButton(HWR.SPIN_UP_INTAKE)
 					&& DriverStation.auxStick.getRawButton(HWR.SPIN_UP_SHOOTER)) {
@@ -248,7 +287,7 @@ public class Shooter {
 		}
 		case INTAKE: {
 			state = "INTAKE";
-			intake(getJoystickAngle());
+			intake();
 			nextState = State.INTAKE;
 			if (!DriverStation.auxStick.getRawButton(HWR.SPIN_UP_INTAKE)) {
 				nextState = State.HOLD;
@@ -258,7 +297,7 @@ public class Shooter {
 		}
 		case SHOOT: {
 			state = "SHOOT";
-			shoot(SmartDashboard.getDouble("TeleOp Shooter Target Speed"), getJoystickAngle());
+			shoot(getSpeed());
 			nextState = State.SHOOT;
 			if (!DriverStation.auxStick.getRawButton(HWR.SPIN_UP_SHOOTER)) {
 				nextState = State.HOLD;
@@ -275,7 +314,7 @@ public class Shooter {
 
 		presentTeleOpState = nextState;
 		SmartDashboard.sendData("Shooter state", state);
-//		report();
+		// report();
 	}
 
 	public void reset() {
@@ -286,24 +325,27 @@ public class Shooter {
 	public void report() {
 		SmartDashboard.sendData("Shooter Encoder Position", angleMotor.getEncPosition());
 		SmartDashboard.sendData("Shooter Position", angleMotor.getPosition());
-		SmartDashboard.sendData("Shooter Angle", angleMotor.getPosition() * POSITION_TO_ANGLE_COEFFICENT);
-		
+		SmartDashboard.sendData("Shooter Angle", angleMotor.getPosition() * 360);
+
 		SmartDashboard.sendData("Left Talon Target", leftMotor.PIDTargetSpeed());
 		SmartDashboard.sendData("Right Talon Target", rightMotor.PIDTargetSpeed());
 		SmartDashboard.sendData("Left Talon Speed", leftMotor.getSpeed());
 		SmartDashboard.sendData("Right Talon Speed", rightMotor.getSpeed());
 		SmartDashboard.sendData("Left Talon Error", leftMotor.PIDErrorSpeed());
 		SmartDashboard.sendData("Right Talon Error", rightMotor.PIDErrorSpeed());
-		
+
 		SmartDashboard.sendData("Left Setpoint", leftMotor.getSetpoint());
 		SmartDashboard.sendData("Left Speed", leftMotor.get());
-		SmartDashboard.sendData("Left Error", leftMotor.getClosedLoopError()/4096.0);
-		
+		SmartDashboard.sendData("Left Error", leftMotor.getClosedLoopError() / 4096.0);
+
 		SmartDashboard.sendData("right Setpoint", rightMotor.getSetpoint());
 		SmartDashboard.sendData("right Speed", rightMotor.get());
-		SmartDashboard.sendData("right Error", rightMotor.getClosedLoopError()/4096.0);
+		SmartDashboard.sendData("right Error", rightMotor.getClosedLoopError() / 4096.0);
 	}
 
+	public void updatePrefs() {
+		inputFilter.setFilterK(SmartDashboard.getDouble("Shooter K"));
+	}
 
 	/**
 	 * Set shooter to speed {@param rpm}
