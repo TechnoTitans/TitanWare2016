b8af54f2a0ca5d2548ffe8bd9114af132557cb13
Sneha Kadiyala <johndoe@example.com>
2016-03-15 17:48:46 -0400

    Cleaned up TechnoTitan

--- src/org/usfirst/frc/team1683/robot/TechnoTitan.java
+++ src/org/usfirst/frc/team1683/robot/TechnoTitan.java
@@ -1,24 +1,20 @@
 
 package org.usfirst.frc.team1683.robot;
-import org.usfirst.frc.team1683.test.*;
+
 import org.usfirst.frc.team1683.autonomous.AutonomousSwitcher;
+import org.usfirst.frc.team1683.driveTrain.MotorGroup;
 import org.usfirst.frc.team1683.driveTrain.TalonSRX;
 import org.usfirst.frc.team1683.driveTrain.TankDrive;
-import org.usfirst.frc.team1683.driverStation.DriverStation;
 import org.usfirst.frc.team1683.driverStation.SmartDashboard;
 import org.usfirst.frc.team1683.pneumatics.Climber;
 import org.usfirst.frc.team1683.pneumatics.Solenoid;
 import org.usfirst.frc.team1683.sensors.Gyro;
 import org.usfirst.frc.team1683.sensors.LinearActuator;
 import org.usfirst.frc.team1683.sensors.QuadEncoder;
-import org.usfirst.frc.team1683.sensors.TiltSensor;
 import org.usfirst.frc.team1683.sensors.PressureReader;
 import org.usfirst.frc.team1683.shooter.Shooter;
 import org.usfirst.frc.team1683.vision.FindGoal;
 
-import edu.wpi.first.wpilibj.CameraServer;
-import edu.wpi.first.wpilibj.Compressor;
-import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.IterativeRobot;
 
 /**
@@ -30,25 +26,20 @@ import edu.wpi.first.wpilibj.IterativeRobot;
  */
 public class TechnoTitan extends IterativeRobot {
 	public static AutonomousSwitcher switcher;
-	public static final double WHEEL_DISTANCE_PER_PULSE = 10;
+//	public static final double WHEEL_DISTANCE_PER_PULSE = 10;
 	public static final boolean LEFT_REVERSE = false;
 	public static final boolean RIGHT_REVERSE = true;
 	public static final double WHEEL_RADIUS = 3.391 / 2;
 	FindGoal findgoal;
 	TankDrive drive;
 	Climber climberPistons;
-	TiltSensor tiltSensor;
-	Solenoid shootPiston;
 	PressureReader pressureReader;
 	// LightRing lightRing;
-	VisionTest visionTest;
 	Shooter shooter;
-	
+
 	LinearActuator actuator;
 
-	Compressor compressor;
-	
-	DigitalInput hallEffect;
+
 
 	/**
 	 * This function is run when the robot is first started up and should be
@@ -57,49 +48,36 @@ public class TechnoTitan extends IterativeRobot {
 	@Override
 	public void robotInit() {
 		// switcher = new AutonomousSwitcher(drive);
-		visionTest=new VisionTest();
+		
 		// GYRO
 		Gyro gyro = new Gyro(HWR.GYRO);
 
 		// DRIVE TRAIN
-		/*
 		TalonSRX leftETalonSRX = new TalonSRX(HWR.LEFT_DRIVE_TRAIN_FRONT_E, LEFT_REVERSE);
 		TalonSRX rightETalonSRX = new TalonSRX(HWR.RIGHT_DRIVE_TRAIN_FRONT_E, RIGHT_REVERSE);
 
 		MotorGroup leftGroup = new MotorGroup(new QuadEncoder(leftETalonSRX, WHEEL_RADIUS),
-				// MotorGroup leftGroup = new MotorGroup(
+		// MotorGroup leftGroup = new MotorGroup(
 				leftETalonSRX, new TalonSRX(HWR.LEFT_DRIVE_TRAIN_BACK, LEFT_REVERSE));
 
 		MotorGroup rightGroup = new MotorGroup(new QuadEncoder(rightETalonSRX, WHEEL_RADIUS),
-				// MotorGroup rightGroup = new MotorGroup(
+		// MotorGroup rightGroup = new MotorGroup(
 				rightETalonSRX, new TalonSRX(HWR.RIGHT_DRIVE_TRAIN_BACK, RIGHT_REVERSE));
 
 		drive = new TankDrive(leftGroup, rightGroup, gyro);
-		*/
 		// END DRIVE TRAIN
 
-		tiltSensor = new TiltSensor(HWR.ACCEL_CHANNEL_X, HWR.ACCEL_CHANNEL_Y);
-
 		actuator = new LinearActuator(HWP.CAN_5, false);
-		
+
 		climberPistons = new Climber(HWR.ClIMB_DEPLOY_CHANNEL, HWR.CLIMB_RETRACT_CHANNEL);
 
-		shootPiston = new Solenoid(HWR.DEFAULT_MODULE_CHANNEL, HWR.SHOOTER_PISTON_CHANNEL);
-//		MotorGroup shooterGroup = new MotorGroup(new TalonSRX(HWR.SHOOTER_LEFT, true),
-//				new TalonSRX(HWR.SHOOTER_RIGHT, true));
-//		TalonSRX leftShooter = new TalonSRX(HWR.SHOOTER_LEFT, false);
-//		TalonSRX rightShooter = new TalonSRX(HWR.SHOOTER_RIGHT, true);
-
-//		pickerUpper = new PickerUpper(shooterGroup);
+		Solenoid shootPiston = new Solenoid(HWR.DEFAULT_MODULE_CHANNEL, HWR.SHOOTER_PISTON_CHANNEL);
 
 		shooter = new Shooter(HWR.SHOOTER_LEFT, HWR.SHOOTER_RIGHT, HWR.ANGLE_MOTOR, shootPiston);
-		
-		hallEffect = new DigitalInput(0);
-		
-		CameraServer server = CameraServer.getInstance();
-		server.setQuality(50);
-		server.startAutomaticCapture("cam1");
-		
+
+		// CameraServer server = CameraServer.getInstance();
+		// server.setQuality(50);
+		// server.startAutomaticCapture("cam1");
 
 	}
 
@@ -120,18 +98,13 @@ public class TechnoTitan extends IterativeRobot {
 
 	@Override
 	public void teleopInit() {
-//		shooter.reset();
+		// shooter.reset();
 	}
 
 	@Override
 	public void teleopPeriodic() {
-		// drive.driveMode();
-		//shooter.shootMode();
-		visionTest.test();
-//		SmartDashboard.sendData("Hall Effect", hallEffect.get());
-//		actuator.convertToInch(SmartDashboard.getDouble("Linear Actuator angle"));
-//		actuator.convertToAngle(SmartDashboard.getDouble("Linear Actuator inch"));
-//		SmartDashboard.sendData("Linear Actuator native position", actuator.getPos() );
+		drive.driveMode();
+		shooter.shootMode();
 
 	}
 
